568a2971bc6b64818bde99de5bf3a7a3d71306f6
gnlwls3274-netizen <gnlwls3274@gmail.com>
2026-05-11 15:04:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function updateClock() {
+
   const clock = document.getElementById("clock");
+
   const now = new Date();
 
   const year = now.getFullYear();
+
   const month = String(now.getMonth() + 1).padStart(2, "0");
+
   const date = String(now.getDate()).padStart(2, "0");
 
   let hours = now.getHours();
+
   const minutes = String(now.getMinutes()).padStart(2, "0");
+
   const seconds = String(now.getSeconds()).padStart(2, "0");
 
   const period = hours >= 12 ? "PM" : "AM";
 
   hours = hours % 12;
+
   hours = hours ? hours : 12;
 
   const formattedHours = String(hours).padStart(2, "0");
@@ -28,5 +35,7 @@ updateClock();
 const themeToggle = document.getElementById("themeToggle");
 
 themeToggle.addEventListener("click", () => {
+
   document.body.classList.toggle("light");
+
 });
